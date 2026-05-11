a41e10c813b00c2a1c9a7cc46f73c7fbb87ce439
iskenderroglu <iskenderroglu@gmail.com>
2026-05-11 16:14:33 +0300

    d

---
+++ handwritten/ramptime.m
@@ -0,0 +1,8 @@
+clc 
+clear all
+
+device_mode=1; % determine if the device is idle (0)
+% supplying power to the system (1)
+% or demanding power from the system (2)
+
+a1
---
+++ newalgo_3.m
@@ -0,0 +1,9 @@
+clc
+clear all
+close all
+
+randomization_time_upperlimit=60; %%
+
+%%random_action_time=randi([1,])
+random_action_delay=;
+random_action_time=;
